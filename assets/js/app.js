3e20fe1d4124114e81a58b79fce63b5daf9d79a7
OULHACI Imad-Eddine <oulimad93@gmail.com>
2018-08-08 13:47:02 +0100

    Revert "commit"
    This reverts commit 4180cd7d8ee01c762641b51b8c7cb2e8751dffab.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,15 @@
 window.$ = window.jQuery = require('jquery')
 require('bootstrap')
 require('slick-carousel')
-require('lightgallery')
-require('masonry-layout')
 
 $(document).ready(() => {
-  $("#projects-slider").slick({
-    autoplay:true,
-    autoplaySpeed:1500,
+  $('#projects-slider').slick({
     centerMode: true,
-    prevArrow: $("#project-prev"),
-    nextArrow: $("#project-next"),
+    prevArrow: $('#project-prev'),
+    nextArrow: $('#project-next'),
+    centerPadding: '100px',
     slidesToShow: 4,
     autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-    {
-      breakpoint: 1580,
-      settings: {
-        slidesToShow: 3,
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-    ]
-  });
+    autoplaySpeed: 3000
+  })
 })
-
-$(document).ready(function(){
-  $('#lightgallery').lightGallery({
-    fullScreen:false,
-    toogleThumb:false,
-    share:false,
-    download:false,
-    autoplayControls:false,
-    actualSize:false,
-    showThumbByDefault:true,
-    selector: '.grid-item-image'
-  });
-});
-
-$(document).ready(function(){
-  $('.grid-masonry').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
-});
